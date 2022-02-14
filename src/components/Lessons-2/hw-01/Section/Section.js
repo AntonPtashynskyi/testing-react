ba66df562f45cac179d02667d692052e8497a276
Anton Ptashynskyi <a.ptashynskyi@gmail.com>
2022-02-14 12:37:15 +0100

    Finished home work 2.1 add style

--- src/components/Lessons-2/hw-01/Section/Section.js
+++ src/components/Lessons-2/hw-01/Section/Section.js
@@ -3,7 +3,7 @@ import "./Section.css";
 export default function Section({ title, children }) {
   return (
     <div className="sectionFeedBack">
-      <h2>{title}</h2>
+      <h2 className="sectionTitle">{title}</h2>
       {children}
     </div>
   );
